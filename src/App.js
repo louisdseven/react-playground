f4bd55c5ee32b4a54e2d2b82f42d2b2175822297
Louis Seven <louisdseven@yahoo.com>
2019-06-13 12:06:04 -0400

    commit after react module #9

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
 import React from 'react';
-import Split from './composition/Split';
+// import Split from './composition/Split';
 import './App.css';
-import Tooltip from './composition/Tooltip';
+// import Tooltip from './composition/Tooltip';
+// import Messages from './Messages';
+import TheDate from './state/TheDate';
 
 // make 2 tooltips here and another inside the App directly
+/*
 const firstTooltip = (
-  <Tooltip color='hotpink' message='tooltip message'>
+  <Tooltip message='tooltip message'>
    ipsum
   </Tooltip>
 )
 const secondTooltip = (
-  <Tooltip color='#126BCC' message='another tooltip message'>
+  <Tooltip message='another tooltip message'>
     officiis
   </Tooltip>
+)
+*/
+const milliseconds = 1000
+setInterval(
+  function callback() {
+    // update the state.date in here
+  },
+  milliseconds
 )
 
 function App() {
   return (
     <main className='App'>
+      <TheDate />
+   {/*
       <Split className='left' flexBasis='2'>
-        This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+        This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
+        
+
+        // make another tooltip directly inside the App
+
+        <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
       </Split>
+
       <Split className='right'>
-        This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
+        This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
       </Split>
+         */}
     </main>
   )
 }
+
+/*
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>YOUR APPLICATION NAME!</h1>
+        <div>Hello!</div>
+      </div>
+      <div>This does not belong here</div>
+    );
+  }
+}
+*/
+
+
 
 export default App;
